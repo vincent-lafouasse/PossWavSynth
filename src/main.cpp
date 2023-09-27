c1ad3a0797d9c63b767acca24f396f3d025e9d46
vincent lafouasse <poss@possmbp.home>
2023-09-27 18:20:38 +0200

    implement Data<T> as an interface between the float repr and the final binary repr

--- src/main.cpp
+++ src/main.cpp
@@ -14,9 +14,9 @@
 #define WAVETABLE_RESOLUTION 1024
 
 // do not use u64
-#define SAMPLE_TYPE u8
-#define BIT_DEPTH 7
-#define SAMPLE_IS_SIGNED false
+#define SAMPLE_TYPE i32
+#define BIT_DEPTH 32
+#define SAMPLE_IS_SIGNED true
 
 template <typename T>
 T clamp(T item, T min, T max)
@@ -66,7 +66,26 @@ void FloatData::write_to_csv(void)
     csv.close();
 }
 
-void FloatData::normalize(void) {}
+void FloatData::normalize(void)
+{
+    float max_abs = 0.0f;
+    float current;
+
+    for (u32 i = 0; i < size; i++)
+    {
+        current = fabs(data[i]);
+        if (isgreater(current, max_abs))
+            max_abs = current;
+    }
+
+    if (max_abs == 0.0f)
+        return;
+
+    for (u32 i = 0; i < size; i++)
+    {
+        data[i] = data[i] / max_abs;
+    }
+}
 
 template <typename T>
 struct Data
@@ -123,9 +142,6 @@ Data<T>::Data(FloatData* float_data, u32 bit_depth_, bool sample_is_signed)
         data[i] = (T)(!sample_is_signed + (float_data->data[i]) * (float)max);
         data[i] = clamp<T>(data[i], min, max);
     }
-
-    std::cout << +min << std::endl;
-    std::cout << +max << std::endl;
 }
 
 template <typename T>
@@ -141,15 +157,13 @@ void Data<T>::write_to_csv(void)
     csv.close();
 }
 
-void fill_data(i32* data, i32 size, Oscillator* oscillator);
-
 int main()
 {
     Wavetable square_wavetable = Wavetable::get_square(WAVETABLE_RESOLUTION);
     Oscillator square_wave = Oscillator::init(&square_wavetable, SAMPLE_RATE);
     square_wave.set_frequency(440.0f);
 
-    float n_seconds = 0.02;
+    float n_seconds = 2;
     u32 n_samples = SAMPLE_RATE * n_seconds * N_CHANNELS;
 
     FloatData float_data(n_samples);
@@ -164,30 +178,16 @@ int main()
 
     float_data.write_to_csv();
 
-    Data<SAMPLE_TYPE> data_(&float_data, BIT_DEPTH, SAMPLE_IS_SIGNED);
-    data_.write_to_csv();
-
-    i32* data = new int[n_samples];
-    fill_data(data, n_samples, &square_wave);
+    Data<SAMPLE_TYPE> data(&float_data, BIT_DEPTH, SAMPLE_IS_SIGNED);
+    data.write_to_csv();
 
     WavHeader header = WavHeader::init(n_samples * (BIT_DEPTH / 8), N_CHANNELS,
                                        SAMPLE_RATE, BIT_DEPTH);
 
-    WavFile wav_file = WavFile::init("wave.wav", &header, data);
+    WavFile wav_file = WavFile::init("wave.wav", &header, data.data);
 
     if (wav_file.write())
         std::cout << "success" << std::endl;
     else
         std::cout << "rip" << std::endl;
-
-    delete[] data;
-}
-
-void fill_data(i32* data, i32 size, Oscillator* oscillator)
-{
-    for (int i = 0; i < size; i++)
-    {
-        data[i] = oscillator->get();
-        oscillator->advance();
-    }
 }
