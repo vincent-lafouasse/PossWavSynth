ad3d54ec7bd9fabf525e2e1dab41af54d8c49a72
vincent lafouasse <poss@possmbp.home>
2023-09-27 16:28:36 +0200

    fill Data with a square wave

--- src/main.cpp
+++ src/main.cpp
@@ -17,9 +17,10 @@
 struct Data
 {
     Data(u32 size_);
+    ~Data();
 
     void write_to_csv(void);
-    ~Data();
+    void normalize(void);
 
     float* data;
     u32 size;
@@ -51,18 +52,30 @@ void Data::write_to_csv(void)
     csv.close();
 }
 
+void Data::normalize(void) {}
+
 void fill_data(i32* data, i32 size, Oscillator* oscillator);
 
 int main()
 {
-    Data data_(24);
-    data_.write_to_csv();
     Wavetable square_wavetable = Wavetable::get_square(WAVETABLE_RESOLUTION);
     Oscillator square_wave = Oscillator::init(&square_wavetable, SAMPLE_RATE);
     square_wave.set_frequency(440.0f);
-    int n_seconds = 4;
 
-    i32 n_samples = SAMPLE_RATE * n_seconds * N_CHANNELS;
+    int n_seconds = 2;
+    u32 n_samples = SAMPLE_RATE * n_seconds * N_CHANNELS;
+
+    Data data_(n_samples);
+
+    for (u32 i = 0; i < n_samples; i++)
+    {
+        data_.data[i] = square_wave.get();
+        square_wave.advance();
+    }
+
+    data_.normalize();
+
+    data_.write_to_csv();
 
     i32* data = new int[n_samples];
     fill_data(data, n_samples, &square_wave);
