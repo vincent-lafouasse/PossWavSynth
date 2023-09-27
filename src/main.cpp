ad2dc9b531facd9224540e2c11df74b63a7d15e7
vincent lafouasse <poss@possmbp.home>
2023-09-27 18:52:36 +0200

    play 2 successive notes with no popping

--- src/main.cpp
+++ src/main.cpp
@@ -18,9 +18,9 @@
 
 int main()
 {
-    Wavetable square_wavetable = Wavetable::get_sine(WAVETABLE_RESOLUTION);
-    Oscillator square_wave = Oscillator::init(&square_wavetable, SAMPLE_RATE);
-    square_wave.set_frequency(440.0f / 1.5f);
+    Wavetable sine_wavetable = Wavetable::get_sine(WAVETABLE_RESOLUTION);
+    Oscillator sine_wave = Oscillator::init(&sine_wavetable, SAMPLE_RATE);
+    sine_wave.set_frequency(440.0f / 1.5f);
 
     float n_seconds = 2;
     u32 n_samples = SAMPLE_RATE * n_seconds * N_CHANNELS;
@@ -29,8 +29,10 @@ int main()
 
     for (u32 i = 0; i < n_samples; i++)
     {
-        float_data.data[i] = square_wave.get();
-        square_wave.advance();
+        float_data.data[i] = sine_wave.get();
+        sine_wave.advance();
+        if (i == n_samples / 2)
+            sine_wave.set_frequency(449.0f);
     }
 
     Data<SAMPLE_TYPE> data(&float_data, BIT_DEPTH, SAMPLE_IS_SIGNED);
