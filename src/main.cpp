e3dd321efa1857d6bae92a70f706c23f0a885d4d
vincent lafouasse <poss@possmbp.home>
2023-09-28 12:53:20 +0200

    play melody from Melody struct (not yet automated)

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <vector>
 
 #include "cool_ints.h"
 #include "data.h"
@@ -16,23 +17,82 @@
 #define BIT_DEPTH 32
 #define SAMPLE_IS_SIGNED true
 
+struct Note
+{
+    Note(float f_, float len_s_);
+
+    u32 get_n_samples(u32 sample_rate);
+
+    float f;
+    float len_s;
+};
+
+Note::Note(float f_, float len_s_)
+{
+    f = f_;
+    len_s = len_s_;
+}
+
+u32 Note::get_n_samples(u32 sample_rate)
+{
+    return len_s * (float)sample_rate;
+}
+
+struct Melody
+{
+    void add_note(float f, float len_s);
+    u32 get_total_n_samples(u32 sample_rate);
+
+    std::vector<Note> notes;
+};
+
+void Melody::add_note(float f, float len_s)
+{
+    notes.push_back(Note(f, len_s));
+}
+
+u32 Melody::get_total_n_samples(u32 sample_rate)
+{
+    u32 n_samples = 0;
+
+    for (size_t i = 0; i < notes.size(); i++)
+    {
+        n_samples += notes.at(i).get_n_samples(sample_rate);
+    }
+
+    return n_samples;
+}
+
 int main()
 {
+    Melody melody;
+    melody.add_note(440.0f, 1.0f);
+    melody.add_note(440.0f * 1.5f, 1.0f);
+
     Wavetable sine_wavetable = Wavetable::get_sine(WAVETABLE_RESOLUTION);
     Oscillator sine_wave = Oscillator::init(&sine_wavetable, SAMPLE_RATE);
-    sine_wave.set_frequency(440.0f / 1.5f);
 
-    float n_seconds = 2;
-    u32 n_samples = SAMPLE_RATE * n_seconds * N_CHANNELS;
+    u32 n_samples = melody.get_total_n_samples(SAMPLE_RATE) * N_CHANNELS;
 
     FloatData float_data(n_samples);
 
-    for (u32 i = 0; i < n_samples; i++)
+    u32 data_index = 0;
+
+    Note a4 = melody.notes.at(0);
+    Note e5 = melody.notes.at(1);
+
+    sine_wave.set_frequency(a4.f);
+    for (u32 i = 0; i < a4.get_n_samples(SAMPLE_RATE); i++, data_index++)
     {
-        float_data.data[i] = sine_wave.get();
+        float_data.data[data_index] = sine_wave.get();
         sine_wave.advance();
-        if (i == n_samples / 2)
-            sine_wave.set_frequency(440.0f);
+    }
+
+    sine_wave.set_frequency(e5.f);
+    for (u32 i = 0; i < e5.get_n_samples(SAMPLE_RATE); i++, data_index++)
+    {
+        float_data.data[data_index] = sine_wave.get();
+        sine_wave.advance();
     }
 
     Data<SAMPLE_TYPE> data(&float_data, BIT_DEPTH, SAMPLE_IS_SIGNED);
