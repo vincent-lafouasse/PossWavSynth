47bc2637d1730ac1a283842ede26221e05dc197b
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2024-04-27 18:46:44 +0200

    clean up dead code

--- src/main.cpp
+++ src/main.cpp
@@ -2,41 +2,15 @@
 
 #include "Data.h"
 #include "Signal.h"
-#include "cool_ints.h"
 #include "melody.h"
 #include "oscillator.h"
 #include "wav.h"
 #include "wavetable.h"
 
 #define SAMPLE_RATE 44100
-#define N_CHANNELS 1
 
-// do not use u64
-#define SAMPLE_TYPE i32
-#define BIT_DEPTH 32
-#define SAMPLE_IS_SIGNED true
-
-#define TEMPO_BPM 120.0f
-#define QUARTER_NOTE_SEC (60.0f / TEMPO_BPM)
-#define EIGTH_TRIPLET_SEC (QUARTER_NOTE_SEC / 3.0f)
-
-class Sample
-{
-   public:
-    virtual void write(FILE* file) = 0;
-    u16 bit_depth;
-    bool is_signed;
-};
-
-class Sample32Bit : Sample
-{
-   public:
-    i32 data;
-    void write(FILE* file){};
-};
-
-Melody a_cool_melody(void);
-Wavetable a_cool_additive_wavetable(void);
+static Melody a_cool_melody(void);
+static Wavetable a_cool_additive_wavetable(void);
 
 int main()
 {
@@ -58,6 +32,10 @@ int main()
 
 Melody a_cool_melody(void)
 {
+constexpr float tempo_bpm = 120.0f;
+constexpr float quarter_note_sec = (60.0f / tempo_bpm);
+constexpr float eigth_triplet_sec = (quarter_note_sec / 3.0f);
+
     const float g4_frequency = get_frequency(4, 7);
     const float a4_frequency = get_frequency(4, 9);
     const float b4_frequency = get_frequency(4, 11);
@@ -65,13 +43,13 @@ Melody a_cool_melody(void)
     const float d5_frequency = get_frequency(5, 2);
 
     Melody melody;
-    melody.add_note(a4_frequency, 2 * EIGTH_TRIPLET_SEC);
-    melody.add_note(b4_frequency, 1 * EIGTH_TRIPLET_SEC);
-    melody.add_note(c5_frequency, 2 * EIGTH_TRIPLET_SEC);
-    melody.add_note(d5_frequency, 1 * EIGTH_TRIPLET_SEC);
-    melody.add_note(b4_frequency, 3 * EIGTH_TRIPLET_SEC);
-    melody.add_note(g4_frequency, 2 * EIGTH_TRIPLET_SEC);
-    melody.add_note(a4_frequency, 4 * EIGTH_TRIPLET_SEC);
+    melody.add_note(a4_frequency, 2 * eigth_triplet_sec);
+    melody.add_note(b4_frequency, 1 * eigth_triplet_sec);
+    melody.add_note(c5_frequency, 2 * eigth_triplet_sec);
+    melody.add_note(d5_frequency, 1 * eigth_triplet_sec);
+    melody.add_note(b4_frequency, 3 * eigth_triplet_sec);
+    melody.add_note(g4_frequency, 2 * eigth_triplet_sec);
+    melody.add_note(a4_frequency, 4 * eigth_triplet_sec);
 
     return melody;
 }
