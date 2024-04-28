b0e2d59e648371babb5f84596aeb0d138d1cd805
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2024-04-28 15:04:44 +0200

    sketch

--- src/wav/Data.cpp
+++ src/wav/Data.cpp
@@ -23,11 +23,19 @@ Data32::Data32(Signal& signal, u32 sample_rate)
 
 void Data32::write(FILE* file, u32 max_bytes) const
 {
-    printf("writing data");
+    printf("writing data to disk\n");
     u32 max_n = std::min<u32>(size, max_bytes / sample_size_byte);
 
     for (u32 i = 0; i < max_n; i++)
         fwrite(data + i, sample_size_byte, 1, file);
+}
+
+void Data32::write_csv(const char* name) const
+{
+    FILE* csv = fopen(name, "w");
+
+    for (u32 i = 0; i < size - 1; i++)
+        fprintf(csv, "");
 }
 
 Data32::~Data32()
