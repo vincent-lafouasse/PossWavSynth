5c6ca810737955cafc440b3699e545f3fb492775
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2024-04-28 15:22:50 +0200

    extract raw time division from midi

--- src/parse_midi/parse_midi.cpp
+++ src/parse_midi/parse_midi.cpp
@@ -8,7 +8,12 @@
 
 float parse_time_division(const HeaderChunk& header)
 {
-    (void)header;
+    u16 raw_time = header.getDivision();
+    u32 bit_size = 8 * sizeof(raw_time);
+    bool top_bit = 1 & (raw_time >> (bit_size - 1));
+    u16 data = raw_time & 0x7FFF;
+    printf("top bit\t%d\n", top_bit);
+    printf("data\t%d\n", data);
     return 0;
 }
 
