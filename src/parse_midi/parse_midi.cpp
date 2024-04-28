f8050b4eee5e0ac5441a3ec66523e87298400ff0
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2024-04-28 16:19:22 +0200

    sketch midi tempo object

--- src/parse_midi/parse_midi.cpp
+++ src/parse_midi/parse_midi.cpp
@@ -1,43 +1,31 @@
 #include <cassert>
 #include <cstring>
+#include <memory>
 
 #include "parse_midi.h"
 
+#include "HeaderChunk.h"
 #include "Midi.h"
 #include "TrackChunk.h"
-
-class MidiTempo
-{
-   public:
-    virtual double get_microsecs_per_tick() = 0;
-};
-
-class PPQMidiTempo : public MidiTempo
-{
-   public:
-    PPQMidiTempo(u16 raw_data);
-
-   private:
-    double bpm;
-};
-class FPSMidiTempo : public MidiTempo
-{
-   public:
-    FPSMidiTempo(u16 raw_data);
-
-   private:
-    double fps;
-};
+#include "MidiTempo.h"
 
 float parse_time_division(const HeaderChunk& header)
 {
-    u16 raw_time = header.getDivision();
-    u32 bit_size = 8 * sizeof(raw_time);
-    bool top_bit = 1 & (raw_time >> (bit_size - 1));
-    u16 data = raw_time & 0x7FFF;
-    printf("top bit\t%d\n", top_bit);
-    printf("data\t%d\n", data);
+    (void)header;
     return 0;
+}
+
+MidiTempo* MidiTempo::parse(const HeaderChunk& header)
+{
+    u16 raw_time = header.getDivision();
+
+    bool top_bit = 1 & (raw_time >> 15);
+    u16 data = raw_time & 0x7FFF;
+
+    if (top_bit)
+        return new FPSMidiTempo(data);
+    else
+        return new PPQMidiTempo(data);
 }
 
 Melody track_to_melody(const TrackChunk& track, float secs_per_tick)
@@ -53,14 +41,13 @@ std::vector<Melody> parse_midi(const char* path)
     Midi mid{path};
 
     const HeaderChunk& header = mid.getHeader();
-    assert(strcmp(header.getName(), "MThd") == 0);
 
-    float secs_per_tick = parse_time_division(header);
+    std::unique_ptr<MidiTempo> tempo(MidiTempo::parse(header));
 
     const std::list<TrackChunk> tracks = mid.getTracks();
     std::vector<Melody> voices{};
 
     for (const TrackChunk& track : tracks)
-        voices.push_back(track_to_melody(track, secs_per_tick));
+        voices.push_back(track_to_melody(track, 0));
     return voices;
 }
