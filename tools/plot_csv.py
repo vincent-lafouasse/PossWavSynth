8721ab096e0ad789d306c1cacf6923fa4ac87047
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2024-04-29 15:54:10 +0200

    load csv into np

--- tools/plot_csv.py
+++ tools/plot_csv.py
@@ -1,8 +1,17 @@
 import csv
+import sys
+import numpy as np
 
 def main():
-    print("hi")
+    if (len(sys.argv) != 2):
+        print(f"Usage ./plot_csv foo.csv")
+        exit(1)
 
+    csv_path = sys.argv[1]
+    print(f"Plotting {csv_path}")
+
+    values = np.genfromtxt(csv_path, delimiter=',')
+    print(values)
 
 if __name__ == '__main__':
     main()
