c1f3191702137a9cff5e2a4a721cd3b2de79b1f2
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2024-04-29 16:12:09 +0200

    plot time in ms

--- tools/plot_csv.py
+++ tools/plot_csv.py
@@ -14,9 +14,15 @@ def main():
 
     values = np.genfromtxt(csv_path, delimiter=',')
     indices = np.arange(0, len(values))
+    time = indices / 44100
+    time = time * 1000 # plot time in ms
+
+    values = values[0:50000]
+    indices = indices[0:50000]
+    time = time[0:50000]
 
     fig, ax = plt.subplots()
-    ax.plot(indices, values)
+    ax.plot(time, values)
     plt.show()
 
 if __name__ == '__main__':
